f4d701967e09eca00bf69ffb20eb5fb6300cc5e6
Kevin Krateros <80607838+kevink2k19@users.noreply.github.com>
2025-08-23 20:16:18 +0630

    Updated package-lock.json

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  'react-native-maps': require.resolve('react-native-maps/lib/index.web.js'),
-};
 
 module.exports = config;
